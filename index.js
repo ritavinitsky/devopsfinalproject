b04d58e4d06c864d9c64e456a2d9a67f9b089db5
neta <netami@ac.sce.ac.il>
2023-06-05 17:22:50 +0300

    eslint addition

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express')
+/* eslint linebreak-style: ["error", "windows"] */
+const express = require('express');
 
-const port = process.env.PORT || 3000
-const app = express()
+const port = process.env.PORT || 3000;
+const app = express();
 
 app.get('/', (req, res) => {
-    console.log("Server Started!")
-    res.sendFile(`${__dirname}/register.html`)
-}
-)
+    console.log('Server Started!');
+    res.sendFile(`${__dirname}/register.html`);
+});
 
-app.listen(port, ()=>{
-    console.log("Server is up")
-})
+app.listen(port, () => {
+    console.log('Server is up');
+});
